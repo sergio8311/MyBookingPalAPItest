7ef61c097ee071c4e3923fad9a5772bd5fdfc54c
SHry <shry@diceus.com>
2017-03-28 17:43:09 +0300

    add major changes

--- MyBookingPalAPItest/ApiTests.cs
+++ MyBookingPalAPItest/ApiTests.cs
@@ -2,6 +2,8 @@
 using System.Net;
 using System.Text;
 using System.Web.Script.Serialization;
+using Newtonsoft.Json.Serialization;
+using NUnit.Framework.Internal;
 
 
 namespace MyBookingPalAPItest
@@ -12,41 +14,30 @@ namespace MyBookingPalAPItest
     using System.Collections.Generic;
     using System.Linq;
 
+
+
     [TestFixture]
     public class ApiTests
     {
-
         [Test]
         public void ApiTest()
         {
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://www.mybookingpal.com/xml/services/json/reservation/products/21980/2017-04-01/2017-04-08?pos=a502d2c65c2f75d3&guests=2&amenity=true&currency=USD&exec_match=false&display_inquire_only=false");
+            
+            var poductName = "Alma-Eiffel Tower";
+            var location = "Paris, France";
+            var nextMonday = ProductHelper.NextMondayDate();
 
-            request.Method = "GET";
-            request.Accept = "application/json";
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            StreamReader reader = new StreamReader(response.GetResponseStream());
-            StringBuilder output = new StringBuilder();
-            output.Append(reader.ReadToEnd());
-            string json = @output.ToString();
-            //var data = (JObject)JsonConvert.DeserializeObject(json);
-            //JObject data = JObject.Parse(json);
-            //JArray quote = (JArray)data["quote"];
-            dynamic dict = JsonConvert.DeserializeObject(json);
-            var searchResponse = dict.search_response.search_quotes.quote[1].productname;
-            //var targetProductName = "Eiffel Balcony";
-            //var productName = Array.FindAll<dynamic>(searchResponse, s => s.Equals(targetProductName));
-            //string productName = searchResponse.productname;
-            //int productId = searchResponse.productid;
+            ////Get location id
+            var locationId = ProductHelper.GetLocationId(location);
 
-
-
-
-            //Array is also possible
-            //string[] result = dict.Select(kv => kv.Value.ToString()).ToArray();
-
-
-            response.Close();
-        }
-     
+            //Get product id
+            var productId = ProductHelper.GetProductId(locationId, poductName);
+           
+            //Get price and quote
+            var productQuote = ProductHelper.GetProcudtQuote(productId, nextMonday);
+            var productPrice = ProductHelper.GetProcudtPrice(productId, nextMonday);
+            //Check that quote equal price
+            Assert.AreEqual(productQuote, productPrice, "Price and Quote are not equal");
+        }   
     }
 }
--- MyBookingPalAPItest/PorcuctData.cs
+++ MyBookingPalAPItest/PorcuctData.cs
@@ -1,9 +1,94 @@
-﻿namespace MyBookingPalAPItest
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace MyBookingPalAPItest
 {
-    public class PorcuctData
+    public static class ProductHelper
     {
-        public string ProductId { get; set; }
-        public string ProductName { get; set; }
-        public int Quote { get; set; }
+        private static readonly string locationUrl = "https://mybookingpal.com/api/location/getinfo?location=";
+        private static readonly string productUrl = "https://www.mybookingpal.com/xml/services/json/reservation/products/";
+        private static readonly string productQuoteUrl = "https://mybookingpal.com/xml/services/json/reservation/quotes?pos=a502d2c65c2f75d3&productid=";
+
+        public static int GetLocationId(string location)
+        {
+            RequestBuilder getLocationJson = new RequestBuilder();
+            var urlLocation = locationUrl + location;
+            var dictLocation = getLocationJson.getPostRequest(urlLocation);
+            return dictLocation.data.ID;
+        }
+
+        public static  int GetProductId(int locationId, string expectedProductName)
+        {
+            var urlGetProductList = productUrl + locationId + "/"+ NextMondayDate() +"/"+ CheckoutDate()+"?pos=a502d2c65c2f75d3&guests=2&amenity=true&currency=USD&exec_match=false&display_inquire_only=false";
+            RequestBuilder getProductListJson = new RequestBuilder();
+            var dictProduct = getProductListJson.getPostRequest(urlGetProductList);
+            var searchResponse = dictProduct.search_response.search_quotes.quote;
+            var productList = (List<SelectableProductListItems>)searchResponse.ToObject<List<SelectableProductListItems>>();
+            var findProductName = productList.FirstOrDefault(x => x.productname.Equals(expectedProductName));
+            return findProductName.productid;
+        }
+
+        public static float GetProcudtQuote(int productId, string monday)
+        {
+            var urlGetProductQuote = productQuoteUrl + productId + "&fromdate=" + monday + "&todate=" + CheckoutDate() + "&currency=USD&adults=2";
+            RequestBuilder getProductDetailsJson = new RequestBuilder();
+            var dictProductDetails = getProductDetailsJson.getPostRequest(urlGetProductQuote);
+            return dictProductDetails.quotes.quote;
+        }
+        public static float GetProcudtPrice(int productId, string monday)
+        {
+            var urlGetProductQuote = productQuoteUrl + productId + "&fromdate=" + monday + "&todate=" + CheckoutDate() + "&currency=USD&adults=2";
+            RequestBuilder getProductDetailsJson = new RequestBuilder();
+            var dictProductDetails = getProductDetailsJson.getPostRequest(urlGetProductQuote);
+            return dictProductDetails.quotes.price;
+        }
+
+
+        public static string NextMondayDate()
+        {
+            DateTime tomorrow = DateTime.Today.AddDays(1);
+            int daysUntilModay = ((int)DayOfWeek.Monday - (int)tomorrow.DayOfWeek + 7) % 7;
+            DateTime nextMonday = tomorrow.AddDays(daysUntilModay);
+            return nextMonday.ToString("yyyy-MM-dd");
+        }
+
+        public static string CheckoutDate()
+        {
+            DateTime tomorrow = DateTime.Today.AddDays(1);
+            int daysUntilModay = ((int)DayOfWeek.Monday - (int)tomorrow.DayOfWeek + 7) % 7;
+            DateTime nextMonday = tomorrow.AddDays(daysUntilModay);
+            DateTime checkoutDateFromMonday = nextMonday.AddDays(7);
+            return checkoutDateFromMonday.ToString("yyyy-MM-dd");
+        }
     }
+
+    public class SelectableProductListItems
+    {
+        public string productname { get; set; }
+        public int productid { get; set; }
+
+    }
+
+    public class RequestBuilder
+    {
+        public dynamic getPostRequest(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.Accept = "application/json";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            StringBuilder output = new StringBuilder();
+            output.Append(reader.ReadToEnd());
+            string json = @output.ToString();
+            return JsonConvert.DeserializeObject(json);          
+        }
+    }
+
+  
 }
