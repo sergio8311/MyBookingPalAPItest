32b0b491ee04e01c9b4d607cbbe9fd25503f64f8
SHry <shry@diceus.com>
2017-03-28 17:57:12 +0300

    remove unused project

--- MyBookingPalAPItest/ApiTests.cs
+++ MyBookingPalAPItest/ApiTests.cs
@@ -1,18 +1,7 @@
-﻿using System.IO;
-using System.Net;
-using System.Text;
-using System.Web.Script.Serialization;
-using Newtonsoft.Json.Serialization;
-using NUnit.Framework.Internal;
-
-
+﻿
 namespace MyBookingPalAPItest
 {
-    using Newtonsoft.Json;
     using NUnit.Framework;
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
 
 
 
@@ -21,13 +10,12 @@ namespace MyBookingPalAPItest
     {
         [Test]
         public void ApiTest()
-        {
-            
-            var poductName = "Alma-Eiffel Tower";
+        {           
+            var poductName = "Apartment Pelleport";
             var location = "Paris, France";
             var nextMonday = ProductHelper.NextMondayDate();
 
-            ////Get location id
+            //Get location id
             var locationId = ProductHelper.GetLocationId(location);
 
             //Get product id
